61b1cf756905cab3cf32983841c894239731563a
Andrew Hayford <emailandrewhayford@gmail.com>
2018-06-09 01:44:58 -0400

    Add TravisCI, test for status route

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+[![Build Status](https://travis-ci.org/hayfordar/rocket-power.svg?branch=master)](https://travis-ci.org/hayfordar/rocket-power)
+
 # rocket-power
-Rust boilerplate for a Rocket/Diesel microservice using MySQL
+## Rust boilerplate for a Rocket/Diesel microservice using MySQL
+
+Requires nightly due to use of rocket-rs.
+
--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,4 +1,17 @@
+use rocket;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[test]
+fn index_test() {
+    let instance = rocket::ignite()
+        .mount("/", routes![index]);
+    let client = rocket::local::Client::new(instance)
+        .unwrap();
+    let mut res = client.get("/").dispatch();
+    assert_eq!(res.body_string()
+        , Some("Hello, world!".into()));
 }
